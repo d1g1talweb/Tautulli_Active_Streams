be1a2887cd9b951ac1c0d782c8deecd33f6a57b1
Richardvaio <113211829+Richardvaio@users.noreply.github.com>
2025-02-09 18:06:46 +0000

    Update const.py

--- custom_components/tautulli_active_streams/const.py
+++ custom_components/tautulli_active_streams/const.py
@@ -2,8 +2,8 @@ import logging
 
 DOMAIN = "tautulli_active_streams"
 
-DEFAULT_SCAN_INTERVAL = 10 
-DEFAULT_SESSION_COUNT = 5 
+DEFAULT_SCAN_INTERVAL = 4 
+DEFAULT_SESSION_COUNT = 6 
 DEFAULT_VERIFY_SSL = True 
 
 LOGGER = logging.getLogger(__package__)
