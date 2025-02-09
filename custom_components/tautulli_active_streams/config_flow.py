e09859e6803039e47091b0d247df9a9f3891156a
Richardvaio <113211829+Richardvaio@users.noreply.github.com>
2025-02-09 18:05:45 +0000

    Update config_flow.py

--- custom_components/tautulli_active_streams/config_flow.py
+++ custom_components/tautulli_active_streams/config_flow.py
@@ -85,7 +85,7 @@ class TautulliOptionsFlowHandler(config_entries.OptionsFlow):
 
     def __init__(self, config_entry):
         """Initialize options flow."""
-        self.config_entry = config_entry
+        self._config_entry = config_entry  # Changed from self.config_entry
         self.options = dict(config_entry.options)
 
     async def async_step_init(self, user_input=None):
